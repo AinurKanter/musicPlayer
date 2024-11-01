6717af16710701b236c5984d70a65461d7c2dd6e
Ainur <ainurkanter@gmail.com>
2024-11-01 16:15:42 +0300

    Начало

---
+++ MusicPlayer3/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace MusicPlayer3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ButStop = new System.Windows.Forms.Button();
+            this.ButRepeat = new System.Windows.Forms.Button();
+            this.ButPlyer = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.NameMusic = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButStop
+            // 
+            this.ButStop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ButStop.BackgroundImage")));
+            this.ButStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ButStop.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.ButStop.ForeColor = System.Drawing.Color.Cornsilk;
+            this.ButStop.Location = new System.Drawing.Point(369, 219);
+            this.ButStop.Name = "ButStop";
+            this.ButStop.Size = new System.Drawing.Size(55, 36);
+            this.ButStop.TabIndex = 0;
+            this.ButStop.UseVisualStyleBackColor = true;
+            this.ButStop.Click += new System.EventHandler(this.ButStop_Click);
+            // 
+            // ButRepeat
+            // 
+            this.ButRepeat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ButRepeat.BackgroundImage")));
+            this.ButRepeat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ButRepeat.Location = new System.Drawing.Point(491, 219);
+            this.ButRepeat.Name = "ButRepeat";
+            this.ButRepeat.Size = new System.Drawing.Size(43, 36);
+            this.ButRepeat.TabIndex = 1;
+            this.ButRepeat.UseVisualStyleBackColor = true;
+            this.ButRepeat.Click += new System.EventHandler(this.ButRepeat_Click);
+            // 
+            // ButPlyer
+            // 
+            this.ButPlyer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ButPlyer.BackgroundImage")));
+            this.ButPlyer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ButPlyer.Location = new System.Drawing.Point(247, 219);
+            this.ButPlyer.Name = "ButPlyer";
+            this.ButPlyer.Size = new System.Drawing.Size(59, 36);
+            this.ButPlyer.TabIndex = 2;
+            this.ButPlyer.UseVisualStyleBackColor = true;
+            this.ButPlyer.Click += new System.EventHandler(this.ButPlyer_Click);
+            // 
+            // NameMusic
+            // 
+            this.NameMusic.AutoSize = true;
+            this.NameMusic.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameMusic.Location = new System.Drawing.Point(335, 108);
+            this.NameMusic.Name = "NameMusic";
+            this.NameMusic.Size = new System.Drawing.Size(111, 25);
+            this.NameMusic.TabIndex = 3;
+            this.NameMusic.Text = "TextMusic";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NameMusic);
+            this.Controls.Add(this.ButPlyer);
+            this.Controls.Add(this.ButRepeat);
+            this.Controls.Add(this.ButStop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButStop;
+        private System.Windows.Forms.Button ButRepeat;
+        private System.Windows.Forms.Button ButPlyer;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label NameMusic;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ MusicPlayer3/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using NAudio.Wave;
+
+namespace MusicPlayer3
+{
+
+
+    public partial class Form1 : Form
+    {
+        private IWavePlayer waveOutDevice;
+        private AudioFileReader audioFileReader;
+        private bool isPlaying = false;
+        private bool isRepeating = false;
+
+        private string filePath = @"C:\Users\Admin\source\repos\MusicPlayer3\Aerosmith - Dream On (Re-record).mp3";
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAudioFile();
+            
+        }
+
+        private void LoadAudioFile()
+        {
+            try
+            {
+                audioFileReader = new AudioFileReader(filePath);
+                waveOutDevice = new WaveOutEvent();
+                waveOutDevice.Init(audioFileReader);
+                waveOutDevice.PlaybackStopped += WaveOutDevice_PlaybackStopped;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке файла: " + ex.Message);
+            }
+        }
+
+
+        private void ButStop_Click(object sender, EventArgs e)
+        {
+            StopPlayback();
+        }
+
+        private void ButRepeat_Click(object sender, EventArgs e)
+        {
+            isRepeating = !isRepeating;
+            ButRepeat.Text = isRepeating ? "Repeat On" : "Repeat Off";
+        }
+
+        private void ButPlyer_Click(object sender, EventArgs e)
+        {
+            if (audioFileReader != null && waveOutDevice != null)
+            {
+                waveOutDevice.Play();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WaveOutDevice_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            if (isRepeating && audioFileReader != null)
+            {
+                audioFileReader.Position = 0;
+                waveOutDevice.Play();
+            }
+        }
+
+        // Остановка воспроизведения и освобождение ресурсов
+        private void StopPlayback()
+        {
+            if (waveOutDevice != null)
+            {
+                waveOutDevice.Stop();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            StopPlayback();
+            base.OnFormClosing(e);
+        }
+
+    }
+}
